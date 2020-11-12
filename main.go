3dd78169ef16808f340703c34d3527be7c5c4ff5
gabogh123 <gabogh99@gmail.com>
2020-11-12 15:53:32 -0600

    Arreglos

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"pokemon-api/database"
 )
 
 func getAllPokemons(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(database.PokemonDb)
+	json.NewEncoder(w).Encode(database.PokemonDbAsValueArray())
 }
 
 func addPokemon(w http.ResponseWriter, r *http.Request) {
@@ -25,12 +27,18 @@ func addPokemon(w http.ResponseWriter, r *http.Request) {
 	}
 	database.PokemonDb[newPokemon.ID] = newPokemon
 	w.WriteHeader(http.StatusOK)
+}
 
 func handleRequests() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "10005"
+	}
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+	myRouter.HandleFunc("/pokemons", addPokemon).Methods("POST")
+	log.Fatal(http.ListenAndServe(":"+port, myRouter))
 }
 
 func commonMiddleware(next http.Handler) http.Handler {
